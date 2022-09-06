c844aca406662c81b5c7a3c5e79b648b6585e433
MinhHieuVux <minhhieuvux@gmail.com>
2022-09-06 09:12:58 +0700

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.turbo_stream { render turbo_stream: turbo_stream.replace('comment_form', partial: 'comments/form', locals: { comment: Comment.new }) }
-        format.html { render partial: 'comments/form', locals: { comment: Comment.new }}
+        format.html { redirect_to project_path(params[:project_id]), locals: { comment: Comment.new }}
       else
         format.turbo_stream { render turbo_stream: turbo_stream.replace('comment_form', partial: 'comments/form', locals: { comment: @comment }) }
         format.html { render partial: 'comments/form', locals: { comment: @comment }}
